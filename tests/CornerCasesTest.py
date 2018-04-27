8639f055b599ee5942e90904ab4f0510446befaa
dpepper <pepper.daniel@gmail.com>
2018-04-27 15:06:33 -0700

    more tests

--- tests/CornerCasesTest.py
+++ tests/CornerCasesTest.py
@@ -31,6 +31,36 @@ class CornerCasesTest(unittest.TestCase):
         self.assertEquals([], rekey([], None, 'v'))
 
 
+    def test_noop(self):
+        data = [1, 2, 3]
+        self.assertEquals(data, rekey(data, None))
+        self.assertEquals(data, rekey(data, None, None))
+
+
+    def test_null(self):
+        data = {
+            None : [1, 2],
+            'b' : [3, 4],
+            'c' : [None, 5]
+        }
+        self.assertEquals(
+            {
+                None : 2,
+                'b' : 4,
+                'c' : 5,
+            },
+            data.rekey(None, 1)
+        )
+        self.assertEquals(
+            {
+                1 : 2,
+                3 : 4,
+                None : 5
+            },
+            data.rekey(0, 1)
+        )
+
+
 
 if __name__ == '__main__':
     unittest.main()
